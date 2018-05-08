e9529e18bd7c09feaf94622bd4ba928fa5ad708d
Kaizeras <hgeorgievkzr@gmail.com>
2018-05-08 18:02:10 +0300

    Removing deprecated attribute

--- src/ngx-image-editor.component.ts
+++ src/ngx-image-editor.component.ts
@@ -68,7 +68,7 @@ declare const Cropper: any;
                         <button mat-icon-button color="accent" (click)="zoomChange(0.1, 'zoomIn')">
                             <mat-icon>zoom_in</mat-icon>
                         </button>
-                        <mat-slider [value]="sliderValue" (input)="zoomChange($event.value)" [thumb-label]="true"></mat-slider>
+                        <mat-slider [value]="sliderValue" (input)="zoomChange($event.value)" thumbLabel></mat-slider>
                         <button mat-icon-button color="accent" (click)="zoomChange(-0.1, 'zoomOut')">
                             <mat-icon>zoom_out</mat-icon>
                         </button>
@@ -163,10 +163,7 @@ declare const Cropper: any;
 
     `,
     styles: [`
-
-        /*
-     * Copyright (c) 2017 Connecto.AI. All rights reserved.
-     */
+      
         .ngx-image-editor-component .photo-editor-header {
             display: flex;
             justify-content: space-around;
