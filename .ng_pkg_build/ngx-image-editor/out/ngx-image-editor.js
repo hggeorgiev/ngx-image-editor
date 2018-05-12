ff125211171c6f7e869bb80e0ec9103848d6fdc5
hggeorgiev <hgeorgievkzr@gmail.com>
2018-05-12 23:43:08 +0300

    refactor: Fixing build

--- .ng_pkg_build/ngx-image-editor/out/ngx-image-editor.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes} checked by tsc
- */
-/**
- * Generated bundle index. Do not edit.
- */
-export { NgxImageEditorModule, NgxImageEditorComponent, EditorOptions, NGX_DEFAULT_RATIOS } from './public_api';
-//# sourceMappingURL=ngx-image-editor.js.map
--- .ng_pkg_build/ngx-image-editor/out/public_api.js
+++
@@ -1,6 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes} checked by tsc
- */
-export { NgxImageEditorModule, NgxImageEditorComponent, EditorOptions, NGX_DEFAULT_RATIOS } from "./ngx-image-editor.module";
-//# sourceMappingURL=public_api.js.map
