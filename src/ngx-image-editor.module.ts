aaaa2cfedcd7e2efc22b8de5054ee3e2ea9e74f6
José Ramón Valenzuela Luna <valenzuela_luna@hotmail.com>
2020-11-24 08:22:32 -0700

    addapt angular material to new versions

--- src/ngx-image-editor.module.ts
+++ src/ngx-image-editor.module.ts
@@ -1,12 +1,17 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import {NgxImageEditorComponent} from './ngx-image-editor.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
-import {
-    MatAutocompleteModule,
-    MatButtonModule, MatButtonToggleModule, MatIconModule, MatInputModule, MatMenuModule, MatProgressSpinnerModule,
-    MatSliderModule,MatDialogModule,
-    MatTabsModule, MatTooltipModule
-} from "@angular/material";
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
